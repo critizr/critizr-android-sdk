ed536ee199d1b7b455a8db609726d309473ae6eb
Souad Alia <souad.alia@gmail.com>
2015-03-04 12:06:28 +0100

    Rename

--- sample/src/com/CritizrSDKExample/MainActivity.java
+++ sample/src/com/CritizrSDKExample/MainActivity.java
@@ -1,36 +1,35 @@
 package com.CritizrSDKExample;
 
+
 import com.CritizrSDK.CritizrListener;
 import com.CritizrSDK.CritizrSDK;
-import com.CustomSDKExample.R;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
-public class MyStoreActivity extends Activity implements CritizrListener{
+public class MainActivity extends Activity implements CritizrListener{
 	
 	public static final String DEBUG_TAG = "CRITIZR_SDK";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.my_store_layout);
+		setContentView(R.layout.activity_main);
 		
 		this.getActionBar().hide();
-	
 	}
+	
+	
+	public void MainClickMethod(View view) {
+		String apiKey = this.getResources().getString(R.string.critizr_api_key);
 
-	public void MyStoreClickMethod(View view) {
-		
-		String apiKey = MyStoreActivity.this.getResources().getString(R.string.critizr_api_key);
 		if(view.getId() == R.id.storelocator_btn){
 			CritizrSDK.getInstance(apiKey).openFeedbackActivity(this, this);
-			
 		}else if(view.getId() == R.id.my_store_btn){
-			String externalPlaceId = "12";    /*External place id I choose for my example - Bolibio Valencienne*/
-			CritizrSDK.getInstance(apiKey).openFeedbackActivity(this, this, externalPlaceId);
+			MainActivity.this.startActivity(new Intent(this, MyStoreActivity.class));
 		}
 		
 	}
@@ -40,6 +39,5 @@ public class MyStoreActivity extends Activity implements CritizrListener{
 		Log.d(DEBUG_TAG, "Feedback sent");
 		
 	}
-	
 	
 }
