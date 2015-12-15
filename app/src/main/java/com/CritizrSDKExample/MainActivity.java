215bc3a8e92a6a32e34ec63eeed0a63f87ce6963
Alexis Camus <alexiscamus60@gmail.com>
2015-12-15 15:35:09 +0100

    Updated sdk and reformatted everything

--- app/src/main/java/com/CritizrSDKExample/MainActivity.java
+++ app/src/main/java/com/CritizrSDKExample/MainActivity.java
@@ -1,50 +1,44 @@
 package com.CritizrSDKExample;
 
-
-import com.CritizrSDK.CritizrListener;
-import com.CritizrSDK.CritizrSDK;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
-public class MainActivity extends Activity implements CritizrListener{
-	
-	public static final String DEBUG_TAG = "CRITIZR_SDK";
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		this.getActionBar().hide();
-	}
-	
-	
-	public void MainClickMethod(View view) {
-		String apiKey = this.getResources().getString(R.string.critizr_api_key);
+import com.CritizrSDK.CritizrListener;
+import com.CritizrSDK.CritizrSDK;
 
-		if(view.getId() == R.id.storelocator_btn){
-			CritizrSDK.getInstance(apiKey).openFeedbackActivity(this, this);
-		}else if(view.getId() == R.id.my_store_btn){
-			MainActivity.this.startActivity(new Intent(this, MyStoreActivity.class));
-		}
-		
-	}
+public class MainActivity extends Activity implements CritizrListener {
 
-	@Override
-	public void onFeedbackSent() {
-		Log.d(DEBUG_TAG, "Feedback sent");
-		
-	}
+    public static final String DEBUG_TAG = "CRITIZR_SDK";
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-	@Override
-	public void onRatingResult(double arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-	
+        this.getActionBar().hide();
+    }
+
+    public void MainClickMethod(View view) {
+        String apiKey = this.getResources().getString(R.string.critizr_api_key);
+
+        if (view.getId() == R.id.storelocator_btn) {
+            CritizrSDK.getInstance(apiKey).openFeedbackActivity(this, this);
+        } else if (view.getId() == R.id.my_store_btn) {
+            MainActivity.this.startActivity(new Intent(this, MyStoreActivity.class));
+        }
+    }
+
+    @Override
+    public void onFeedbackSent() {
+        Log.d(DEBUG_TAG, "Feedback sent");
+    }
+
+    @Override
+    public void onRatingResult(double arg0) {
+        // TODO Auto-generated method stub
+
+    }
 }
