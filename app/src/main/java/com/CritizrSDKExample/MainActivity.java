eccc73a5e0e3ca7d78568029dc20dc8b9bcc481b
Jean-Philippe DESCAMPS <jeanphilippe.descamps@gmail.com>
2016-05-23 13:50:06 +0200

    Remise à niveau du sdk android.

--- app/src/main/java/com/CritizrSDKExample/MainActivity.java
+++ app/src/main/java/com/CritizrSDKExample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.CritizrSDKExample;
 
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,36 +10,40 @@ import android.view.View;
 import com.CritizrSDK.CritizrListener;
 import com.CritizrSDK.CritizrSDK;
 
-public class MainActivity extends Activity implements CritizrListener {
+public class MainActivity extends Activity implements CritizrListener{
+	
+	public static final String DEBUG_TAG = "CRITIZR_SDK";
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		this.getActionBar().hide();
+	}
+	
+	
+	public void MainClickMethod(View view) {
+		String apiKey = this.getResources().getString(R.string.critizr_api_key);
 
-    public static final String DEBUG_TAG = "CRITIZR_SDK";
+		if(view.getId() == R.id.storelocator_btn){
+			CritizrSDK.getInstance(apiKey).openFeedbackActivity(this, this);
+		}else if(view.getId() == R.id.my_store_btn){
+			MainActivity.this.startActivity(new Intent(this, MyStoreActivity.class));
+		}
+		
+	}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	@Override
+	public void onFeedbackSent() {
+		Log.d(DEBUG_TAG, "Feedback sent");
+	}
 
-        this.getActionBar().hide();
-    }
 
-    public void MainClickMethod(View view) {
-        String apiKey = this.getResources().getString(R.string.critizr_api_key);
-
-        if (view.getId() == R.id.storelocator_btn) {
-            CritizrSDK.getInstance(apiKey).openFeedbackActivity(this, this);
-        } else if (view.getId() == R.id.my_store_btn) {
-            MainActivity.this.startActivity(new Intent(this, MyStoreActivity.class));
-        }
-    }
-
-    @Override
-    public void onFeedbackSent() {
-        Log.d(DEBUG_TAG, "Feedback sent");
-    }
-
-    @Override
-    public void onRatingResult(double arg0) {
-        // TODO Auto-generated method stub
-
-    }
+	@Override
+	public void onRatingResult(double arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
--- app/src/main/java/com/CritizrSDKExample/MyStoreActivity.java
+++ app/src/main/java/com/CritizrSDKExample/MyStoreActivity.java
@@ -8,40 +8,42 @@ import android.view.View;
 import com.CritizrSDK.CritizrListener;
 import com.CritizrSDK.CritizrSDK;
 
-public class MyStoreActivity extends Activity implements CritizrListener {
-
-    public static final String DEBUG_TAG = "CRITIZR_SDK";
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.my_store_layout);
-
-        this.getActionBar().hide();
-
+public class MyStoreActivity extends Activity implements CritizrListener{
+	
+	public static final String DEBUG_TAG = "CRITIZR_SDK";
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.my_store_layout);
+		
+		this.getActionBar().hide();
+		
 		/* To get place Rating */
-        String apiKey = MyStoreActivity.this.getResources().getString(R.string.critizr_api_key);
-        String externalPlaceId = "12";
-        CritizrSDK.getInstance(apiKey).getPlaceRating(externalPlaceId, this);
-    }
+		String apiKey = MyStoreActivity.this.getResources().getString(R.string.critizr_api_key);
+		String externalPlaceId = "12";
+		CritizrSDK.getInstance(apiKey).getPlaceRating(externalPlaceId, this);
+	}
+	
+	public void MyStoreClickMethod(View view) {
+		String apiKey = MyStoreActivity.this.getResources().getString(R.string.critizr_api_key);
+		if(view.getId() == R.id.storelocator_btn){
+			CritizrSDK.getInstance(apiKey).openFeedbackActivity(this, this);
+			
+		}else if(view.getId() == R.id.my_store_btn){
+			String externalPlaceId = "12";    /*External place id I choose for my example - Bolibio Valencienne*/
+			CritizrSDK.getInstance(apiKey).openFeedbackActivity(this, this, externalPlaceId);
+		}
+	}
 
-    public void MyStoreClickMethod(View view) {
-        String apiKey = MyStoreActivity.this.getResources().getString(R.string.critizr_api_key);
-        if (view.getId() == R.id.storelocator_btn) {
-            CritizrSDK.getInstance(apiKey).openFeedbackActivity(this, this);
-        } else if (view.getId() == R.id.my_store_btn) {
-            String externalPlaceId = "12";    /*External place id I choose for my example - Bolibio Valencienne*/
-            CritizrSDK.getInstance(apiKey).openFeedbackActivity(this, this, externalPlaceId);
-        }
-    }
+	@Override
+	public void onFeedbackSent() {
+		Log.d(DEBUG_TAG, "Feedback sent");
+		
+	}
 
-    @Override
-    public void onFeedbackSent() {
-        Log.d(DEBUG_TAG, "Feedback sent");
-    }
-
-    @Override
-    public void onRatingResult(double rating) {
-        Log.d(DEBUG_TAG, "Place Rating is " + rating);
-    }
+	@Override
+	public void onRatingResult(double rating) {
+		Log.d(DEBUG_TAG, "Place Rating is "+rating);		
+	}
 }
